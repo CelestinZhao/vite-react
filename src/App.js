a59de803ca66bb98710be113ab6b3474da4d73a7
caryzhao <caryzhao@tencent.com>
2024-06-06 16:39:35 +0800

    引入布局

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { BrowserRouter, useRoutes } from 'react-router-dom';
+import { Layout } from 'tdesign-react';
 import router from './router';
+
+const { Header, Content, Aside } = Layout;
 
 function App() {
   const Router = () => useRoutes(router);
 
   return (
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <Layout className="site-layout">
+      <Header/>
+      <Layout>
+        <Aside/>
+        <Content className="site-layout-content">
+          <BrowserRouter>
+            <Router/>
+          </BrowserRouter>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 

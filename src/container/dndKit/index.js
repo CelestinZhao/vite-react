931e18fef0dbc414812a2b493b5f606dac59e52f
caryzhao <caryzhao@tencent.com>
2024-06-14 17:02:50 +0800

    dnd kit修改

--- src/container/dndKit/index.js
+++ src/container/dndKit/index.js
@@ -1,39 +1,35 @@
 import React, { useState } from 'react';
-import { SortableList } from './components/SortableList';
-import './SortableItem.css';
-import './SortableList.css';
+import { Button } from 'tdesign-react';
+import { MoveIcon } from 'tdesign-icons-react';
+import DndKit from '../../utils/dndKit';
+import styles from './index.module.css';
 
 function DragItem(props) {
   const { item, dragHandleProps } = props;
 
   return (
-    <div className="SortableItem">
+    <div className={styles.SortableItem}>
       {item.id}
       <div {...dragHandleProps}>
-        <button className="DragHandle">
-          <svg viewBox="0 0 20 20" width="12">
-            <path
-              d="M7 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 2zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 7 14zm6-8a2 2 0 1 0-.001-4.001A2 2 0 0 0 13 6zm0 2a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 8zm0 6a2 2 0 1 0 .001 4.001A2 2 0 0 0 13 14z"></path>
-          </svg>
-        </button>
+        <Button variant="text" shape="square" icon={<MoveIcon />}/>
       </div>
     </div>
   )
 }
 
-export default function DndKit() {
+export default function DndKitDemo() {
   const [items, setItems] = useState([...new Array(10)].map((_, index) => ({ id: index + 1 })));
 
   return (
     <div style={{ maxWidth: 500, margin: "30px auto" }}>
-      <SortableList
-        className="SortableList"
+      <DndKit
+        className={styles.SortableList}
         items={items}
         onChange={setItems}
         renderItem={(item) => (
-          <SortableList.Item id={item.id}>
+          <DndKit.Item id={item.id}>
             <DragItem item={item}/>
-          </SortableList.Item>
+          </DndKit.Item>
         )}
       />
     </div>
---
+++ src/utils/dndKit/index.js
@@ -0,0 +1,68 @@
+import React, { useMemo, useState } from 'react';
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+} from '@dnd-kit/sortable';
+import { SortableItem } from './SortableItem';
+import { SortableOverlay } from './SortableOverlay';
+
+export default function DndKit(props) {
+  const { className, items, onChange, renderItem } = props;
+  const [active, setActive] = useState(null);
+  const activeItem = useMemo(() => (
+    items.find((item) => item.id === active?.id)
+  ), [active, items]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+  );
+
+  const onDragStart = ({ active }) => {
+    setActive(active);
+  };
+
+  const onDragEnd = ({ active, over }) => {
+    if (over && active.id !== over?.id) {
+      const activeIndex = items.findIndex(({ id }) => id === active.id);
+      const overIndex = items.findIndex(({ id }) => id === over.id);
+      onChange(arrayMove(items, activeIndex, overIndex));
+    }
+    setActive(null);
+  };
+
+  const onDragCancel = () => {
+    setActive(null);
+  };
+
+  return (
+    <DndContext
+      sensors={sensors}
+      onDragStart={onDragStart}
+      onDragEnd={onDragEnd}
+      onDragCancel={onDragCancel}
+    >
+      <SortableContext items={items}>
+        <ul className={className} role="application">
+          {
+            items.map((item) => (
+              <React.Fragment key={item.id}>{renderItem(item)}</React.Fragment>
+            ))
+          }
+        </ul>
+      </SortableContext>
+      <SortableOverlay>
+        {activeItem ? renderItem(activeItem) : null}
+      </SortableOverlay>
+    </DndContext>
+  );
+}
+
+DndKit.Item = SortableItem;

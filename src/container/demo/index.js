fab3ace35966d9a7377875c282df5d1e2f6f5e98
caryzhao <caryzhao@tencent.com>
2024-06-17 16:27:17 +0800

    demo修改

--- src/container/demo/index.js
+++ src/container/demo/index.js
@@ -1,43 +1,42 @@
 import React from 'react';
-import { useLocation, useNavigate, useParams, useSearchParams } from 'react-router-dom';
-import { Button, Space } from 'tdesign-react';
-import { get } from 'lodash';
-import axios from 'axios';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button, message, Space, Upload } from 'tdesign-react';
+// import { get } from 'lodash';
 import { getOcr } from '../../utils/ocrUtils';
 import styles from './demo.module.less';
 import quotaModule from './quotaModule.xlsx';
 
-export const onFileChange = async (e) => {
-	const files = e.target.files || e.dataTransfer.files;
-	await getOcr(files[0]);
-	// 通过FormData将文件转成二进制数据
+const requestMethod = async (file) => {
+	// const files = e.target.files || e.dataTransfer.files;
+	await getOcr(file.raw);
 	const formData = new FormData();
-	// 将文件转二进制
-	for (let i = 0; i < files.length; i++) {
-		formData.append(`file[${i}]`, files[i]);
-	}
-	formData.append('length', String(files.length));
+	formData.append('file[0]', file.raw);
+	formData.append('length', 1);
 
-	await axios.post('/api/v1/proxy/manualQuotaUpload', formData, {
-		headers: {
-			'Content-Type': 'multipart/form-data',
-		},
+	return ({
+		status: 'success',
 	});
 };
 
 function Demo() {
-	// const location = useLocation();
+	const location = useLocation();
 	const navigate = useNavigate();
 
 	const onClick = () => {
 		navigate('/', { state: { id: 'demo' } });
-	}
+	};
+	console.log(location.search);
 
 	return (
 		<Space direction="vertical">
 			<Button className={styles.demo} onClick={onClick}>主页</Button>
 			<a href={quotaModule} download={'新增模板.xlsx'}>模板下载</a>
-			<input type="file" accept="image/*" onChange={onFileChange}/>
+			<Upload
+				requestMethod={requestMethod}
+				placeholder="图片orc识别"
+				onSuccess={() => message.success('识别成功')}
+				onFail={() => message.warning('识别失败')}
+			/>
 		</Space>
 	);
 }
